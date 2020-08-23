3b910983c389f325abf077ebfea58e4724c5fab0
Joe <jeric720@icloud.com>
2020-08-23 11:53:21 -0400

    added js to fetch data from unsplash API

---
+++ script.js
@@ -0,0 +1,19 @@
+// Unsplash API
+
+const count = 10;
+const apiKey =  '0DNsBSfJvVK_tMZVtL8XVtirqjrx_fRZBKZrDeijPAs';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos()  {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data); 
+    } catch (error) {
+        // catch error here
+    } 
+}
+
+// On load
+getPhotos();
