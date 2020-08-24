9c2adef1e4168c5474bc285f9c1d4f9a1e892f19
Joe <jeric720@icloud.com>
2020-08-23 23:37:28 -0400

    added infinite scroll js functionality

--- script.js
+++ script.js
@@ -22,14 +22,12 @@ function displayPhotos() {
     photosArray.forEach((photo) => {
         // create <a> to link to Unsplash 
         const item = document.createElement('a');
-
         setAttributes(item, {
             href: photo.links.html,
             target: '_blank',
         });
         // create <img> for photo
         const img = document.createElement('img');
-
         setAttributes(img, {
             src: photo.urls.regular,
             alt: photo.alt_description,
@@ -39,7 +37,6 @@ function displayPhotos() {
         item.appendChild(img);
         imageContainer.appendChild(item); 
     });
-
 }
 
 // Get photos from Unsplash API
@@ -52,6 +49,13 @@ async function getPhotos()  {
         // catch error here
     } 
 }
+
+// Check to see if scrolling near bottom and if so load more photos
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+        getPhotos();
+    }
+})
 
 // On load
 getPhotos();
